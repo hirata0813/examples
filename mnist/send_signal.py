13f3a17d56ec217e42ae15d4eebbde43ca52dfef
hirata0813 <hirata2024@s.okayama-u.ac.jp>
2024-12-11 18:25:25 +0900

    Update script

--- mnist/send_signal.py
+++ mnist/send_signal.py
@@ -6,6 +6,9 @@ import subprocess
 import time
 import sys
 
+# 第一引数：シグナルを送る対象
+# 第二引数：シグナルを送る間隔
+
 def stop_gpu_process(target):
     try:
         # 実行中のプロセスを検索
@@ -56,6 +59,31 @@ def cont_gpu_process(target):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def kill_gpu_process(target):
+    try:
+        # 実行中のプロセスを検索
+        result = subprocess.run(["pgrep", "-f", target], capture_output=True, text=True)
+        pids = result.stdout.strip().split("\n")
+        current_pid = os.getpid() # 自身のPIDを取得
+        pids = [pid for pid in pids if pid and int(pid) != current_pid] # pidsから自身のPIDを除外
+        
+        if not pids or pids == ['']:
+            print("GPU process is not running.")
+            return
+        
+        # 見つかったすべてのプロセスを再開
+        for pid in pids:
+            print(f"Killing GPU process with PID: {pid}")
+            send_signal(pid, signal.SIGKILL)
+        print("GPU process has been killed.")
+    
+        # SIGKILL送信時のタイムスタンプを取得
+        with open("sigkill.log","a") as f:
+            print("{}".format(time.time()), file=f)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 def send_signal(pid, signal):
     try:
         os.kill(int(pid), signal)
@@ -63,17 +91,41 @@ def send_signal(pid, signal):
         if e.errno != errno.ESRCH: #「No such process」じゃない場合
             raise e
 
+def stop_nvidia_smi():
+    try:
+        # 実行中のプロセスを検索
+        result = subprocess.run(["pgrep", "-f", "nvidia-smi"], capture_output=True, text=True)
+        pids = result.stdout.strip().split("\n")
+        
+        if not pids or pids == ['']:
+            print("nvidia-smi is not running.")
+            return
+        
+        # 見つかったすべてのプロセスを停止
+        for pid in pids:
+            print(f"Stopping nvidia-smi process with PID: {pid}")
+            subprocess.run(["kill", "-TERM", pid])
+        print("nvidia-smi has been stopped.")
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 def control_gpu_process(target, sleeptime):
     # ここでGPUプログラムの停止・再開を行う
     stop_gpu_process(target)
     time.sleep(sleeptime)
     cont_gpu_process(target)
+    time.sleep(sleeptime)
+    kill_gpu_process(target)
+    time.sleep(5)
+    stop_nvidia_smi()
 
 def main():
     args = sys.argv
     target = args[1]
     sleeptime = int(args[2])
     control_gpu_process(target, sleeptime)
+
 
 if __name__ == '__main__':
     # GPU プログラムはこのプログラムを実行するだけで良い
